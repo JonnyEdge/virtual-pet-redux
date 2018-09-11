7dd91be38416e6608d9205b7b0e04b207d95ae46
Jonny Edge <jonnyedge@outlook.com>
2018-09-11 21:18:37 +0100

    Fleshing out growUp function with varying hunger and fitness levels. Creating a walk function to improve fitness level, to a level cap of 10.

--- __tests__/pet-redux.test.js
+++ __tests__/pet-redux.test.js
@@ -13,10 +13,41 @@ describe('name', () => {
   });
 });
 
-describe('age', () => {
-  it ('adds 1 to the default age of 0 every time it is called', () => {
+describe('growUp', () => {
+  it('adds 1 to the initial age of 0 every time it is called', () => {
     const pet = new Pet('OlDirtyBastard');
     pet.growUp();
     expect(pet.age).toEqual(1);
+  });
+  it('adds 5 to the initial hunger level of 0 every time it is called', () => {
+    const pet = new Pet('RZA');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+  it('subtracts 3 from the initial fitness level of 10 every time it is called', () => {
+    const pet = new Pet('MethodMan');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
+});
+
+describe('walk', () => {
+  it('adds 4 to the fitness level, to a level cap of 10 - starting fitness 10', () => {
+    const pet = new Pet('Raekwon');
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  });
+  it('adds 4 to the fitness level, to a level cap of 10 - starting fitness 7', () => {
+    const pet = new Pet('GZA');
+    pet.growUp();
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  });
+  it('adds 4 to the fitness level, to a level cap of 10 - starting fitness 4', () => {
+    const pet = new Pet('GZA');
+    pet.growUp();
+    pet.growUp();
+    pet.walk();
+    expect(pet.fitness).toEqual(8);
   });
 });
--- src/pet-redux.js
+++ src/pet-redux.js
@@ -1,10 +1,23 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
   this.name = name;
   this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
 }
 
 Pet.prototype.growUp = function growUp() {
   this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
+};
+
+Pet.prototype.walk = function walk() {
+  this.fitness += 4;
+  if (this.fitness > MAXIMUM_FITNESS) {
+    this.fitness = MAXIMUM_FITNESS;
+  }
 };
 
 module.exports = Pet;
